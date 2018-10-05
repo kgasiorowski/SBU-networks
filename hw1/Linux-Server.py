5d48a226c4ef7a3b9c8a8e0d85495f613fb683b7
Kuba Gasiorowski <Kuba.gasiorowski@stonybrook.edu>
2018-10-04 23:39:41 -0400

    Refactor port selection and validation code

--- hw1/Linux-Server.py
+++ hw1/Linux-Server.py
@@ -7,21 +7,12 @@
 import socket
 import sys
 from enum import Enum
+from contextlib import closing
 
 # Server stuff
 HOST = ''
 DEFAULT_PORT = 8000
-
-if len(sys.argv) == 1:
-    print('No port passed, using default {0}'.format(DEFAULT_PORT))
-    PORT = DEFAULT_PORT
-else:
-    try:
-        PORT = int(sys.argv[1])
-    except:
-        print('Error: invalid port passed in. Using default of {0}'.format(DEFAULT_PORT))
-        PORT = DEFAULT_PORT
-
+PORT = DEFAULT_PORT
 HTML_VERSION = 1.1
 
 # Dictionary for all our response codes
@@ -49,125 +40,182 @@ DICTIONARY = {}
 def send_response(client, responseCode):
     client.send('HTTP/{0} {1} {2}\n'.format(HTML_VERSION, responseCode, RESPONSE_CODES[responseCode]).encode())
 
-
+# Main program executes here
 if __name__ == '__main__':
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Socket initialized")
-
-
+    # Catches keyboard interrupts
     try:
-        sock.bind((HOST, PORT))
-        print("Socket bound to port {0}".format(PORT))
-    except PermissionError as pe:
-        print("Error - the selected port was denied by the system. Continuing with default of {0}".format(DEFAULT_PORT))
-        sock.bind((HOST, DEFAULT_PORT))
 
-    sock.listen(5)
-    print("Socket now listening")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Socket initialized...")
 
-    # Continue to accept connections until the loop is broken out of
-    while True:
+        # First try to parse a port passed in via cmd line args.
+        try:
+            
+            PORT = int(sys.argv[1])
+            print('Using port {0}'.format(PORT))
+        
+        except:
 
-        # Wait for a connection
-        conn, addr = sock.accept()
-        print("Connected to {0}".format(addr))
-        conn.send(b"Enter input:")
+            PORT = DEFAULT_PORT
+            print('Using port {0} (default)'.format(PORT))
 
-        cmd = ''
+        # PORT now has either the custom port or the default
+        # Now try to bind the socket
+        validPortSelected = False
 
-        # Continue to get input on the current connection until
-        # QUIT or OFF breaks out of the loop
+        try:
+
+            # If the socket correctly binds, then skip the while loop and continue
+            print('Trying to bind port {0}...'.format(PORT))
+            sock.bind((HOST, PORT))
+            validPortSelected = True
+
+        except:
+
+            # If not, then prompt the user for a new port
+            print("Error: port {0} could not be bound. Please enter a different port".format(PORT))
+
+        # Continue prompting the user for ports until the socket correctly binds
+        while not validPortSelected:
+
+            try:
+                
+                PORT = int(input('Enter a port:'))
+                print('Trying to bind port {0}...'.format(PORT))
+                sock.bind((HOST, PORT))
+                validPortSelected = True
+
+            except ValueError:
+                print("Enter a number please")
+            except:
+                print("Socket could not be bound, enter a different port")
+
+        print("Success!")    
+
+        sock.listen(5)
+        print("Socket now listening...")
+
+        # Continue to accept connections until the loop is broken out of
         while True:
+
+            # Wait for a connection
+            conn, addr = sock.accept()
+            print("Connected to {0}".format(addr))
+            conn.send(b"Enter input:")
 
             cmd = ''
 
-            # Read data character by character until the terminating char is read
-            cmd = conn.recv(1024).decode()
+            # Continue to get input on the current connection until
+            # QUIT or OFF breaks out of the loop
+            while True:
 
-            # Echo the entire command in the server
-            print(cmd, end='')
+                cmd = ''
 
-            # Split the command into tokens
-            tokens = cmd.split()
+                # Read data character by character until the terminating char is read
+                cmd = conn.recv(1024).decode()
 
-            tokens[0] = tokens[0].upper()
+                # Echo the entire command in the server
+                print(cmd, end='')
 
-            # If no tokens are parsed, return UNSUPPORTED
-            if len(tokens) < 1:
-                send_response(conn, 220)
-                continue
+                # Split the command into tokens
+                tokens = cmd.split()
 
-            # The actual command should be the first token
-            cmd = tokens[0]
+                tokens[0] = tokens[0].upper()
 
-            if cmd == Command.GET.value:
-
-                # Send BAD REQUEST if there is not enough tokens
-                if len(tokens) <= 1:
-                    send_response(conn, 400)
-
-                # Return not found if the requested key is not found
-                elif tokens[1] not in DICTIONARY:
-                    send_response(conn, 404)
-
-                # Or send OK and the dictionary value retrieved
-                else:
-
-                    send_response(conn, 200)
-                    conn.send('\n{0}\n'.format(DICTIONARY.get(tokens[1])).encode())
-
-            elif cmd == Command.PUT.value:
-
-                # BAD REQUEST if there are not exactly 3 tokens
-                if len(tokens) != 3:
-                    send_response(conn, 400)
-
-                # Otherwise update the dictionary and send OK
-                else:
-
-                    DICTIONARY[tokens[1]] = tokens[2]
-                    send_response(conn, 200)
-
-
-            elif cmd == Command.CLEAR.value:
-
-                # Dictionary should always be cleared and send OK
-                DICTIONARY.clear()
-                send_response(conn, 200)
-
-            elif cmd == Command.DELETE.value:
-
-                # There must be at least 2 tokens, otherwise send BAD REQUEST
-                if len(tokens) <= 1:
-                    send_response(conn, 400)
+                # If no tokens are parsed, return UNSUPPORTED
+                if len(tokens) < 1:
+                    send_response(conn, 220)
                     continue
 
-                # IF the key exists in dictionary, delete it
-                if tokens[1] in DICTIONARY:
-                    del DICTIONARY[tokens[1]]
+                # The actual command should be the first token
+                cmd = tokens[0]
 
-                # Always send OK
-                send_response(conn, 200)
+                if cmd == Command.GET.value:
 
-            elif cmd == Command.QUIT.value:
+                    # Send BAD REQUEST if there is not enough tokens
+                    if len(tokens) <= 1:
+                        send_response(conn, 400)
 
-                # Send OK, close this connection, and wait for a new one
-                send_response(conn, 200)
-                conn.close()
-                break
+                    # Return not found if the requested key is not found
+                    elif tokens[1] not in DICTIONARY:
+                        send_response(conn, 404)
 
-            elif cmd == Command.OFF.value:
+                    # Or send OK and the dictionary value retrieved
+                    else:
 
-                # Send OK, close the connection and socket, and shut down the server
-                send_response(conn, 200)
-                conn.send(b'Server shutting down!\n')
-                print('Client sent the off signal. Goodbye!')
-                conn.close()
-                sock.close()
-                exit(0)
+                        send_response(conn, 200)
+                        conn.send('\n{0}\n'.format(DICTIONARY.get(tokens[1])).encode())
 
-            else:
+                elif cmd == Command.PUT.value:
 
-                # Otherwise, send UNSUPPORTED
-                send_response(conn, 220)
+                    # BAD REQUEST if there are not exactly 3 tokens
+                    if len(tokens) != 3:
+                        send_response(conn, 400)
+
+                    # Otherwise update the dictionary and send OK
+                    else:
+
+                        DICTIONARY[tokens[1]] = tokens[2]
+                        send_response(conn, 200)
+
+
+                elif cmd == Command.CLEAR.value:
+
+                    # Dictionary should always be cleared and send OK
+                    DICTIONARY.clear()
+                    send_response(conn, 200)
+
+                elif cmd == Command.DELETE.value:
+
+                    # There must be at least 2 tokens, otherwise send BAD REQUEST
+                    if len(tokens) <= 1:
+                        send_response(conn, 400)
+                        continue
+
+                    # IF the key exists in dictionary, delete it
+                    if tokens[1] in DICTIONARY:
+                        del DICTIONARY[tokens[1]]
+
+                    # Always send OK
+                    send_response(conn, 200)
+
+                elif cmd == Command.QUIT.value:
+
+                    # Send OK, close this connection, and wait for a new one
+                    send_response(conn, 200)
+                    conn.close()
+                    break
+
+                elif cmd == Command.OFF.value:
+
+                    # Send OK, close the connection and socket, and shut down the server
+                    send_response(conn, 200)
+                    conn.send(b'Server shutting down!\n')
+                    print('Client sent the off signal. Goodbye!')
+                    conn.close()
+                    sock.close()
+                    exit(0)
+
+                else:
+
+                    # Otherwise, send UNSUPPORTED
+                    send_response(conn, 220)
+
+    # User pressed ctrl-c. Close everything and exit 
+    except KeyboardInterrupt:
+        print('User manually quit.')
+        try:
+            conn.close()
+            print("Connection closed...")
+        except NameError:
+            pass
+
+        try:
+            sock.close()
+            print("Socket closed...")
+        except NameError:
+            pass        
+
+        print("Goodbye!")
+        exit(0) 
